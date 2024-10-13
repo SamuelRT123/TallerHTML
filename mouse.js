7a100301eb2ed746aeb9e16ca151738011f6e883
samrt123 <se.rodriguez2@uniandes.edu.co>
2024-10-13 04:47:37 -0500

    Front-Page Terminada

---
+++ mouse.js
@@ -0,0 +1,7 @@
+const cursor= document.querySelector('.mouse');
+document.addEventListener('mousemove', e=> {
+  cursor.setAttribute("style", "top: " + (e.pageY -33) + "px; left: "+(e.pageX -33)+ "px;");
+});
+$(document).ready(function(){
+    $('#exampleModal').modal('show');
+}); 
