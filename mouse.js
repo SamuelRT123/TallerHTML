ce7049f0c99624f6de0fffc768751600238174be
samrt123 <se.rodriguez2@uniandes.edu.co>
2024-10-13 19:35:39 -0500

    Musica y modelos 3d

--- mouse.js
+++ mouse.js
@@ -4,31 +4,4 @@ document.addEventListener('mousemove', e=> {
 });
 $(document).ready(function(){
     $('#exampleModal').modal('show');
-}); 
-
-document.getElementById("play-7years").addEventListener("click", function() {
-  var audio = document.getElementById("sonido1");
-  audio.play();
 });
-
-  const boton1 = document.getElementById('play-7years');
-  const sonido1 = document.getElementById('sonido1');
-
-  boton1.addEventListener('click', () => {
-    sonido1.play();
-  });
-
-  const boton2 = document.getElementById('play-InsideOut');
-  const sonido2 = document.getElementById('sonido2');
-
-  boton2.addEventListener('click', () => {
-    sonido2.play();
-  });
-
-
-  const boton3 = document.getElementById('play-OneLastTime');
-  const sonido3 = document.getElementById('sonido3');
-
-  boton3.addEventListener('click', () => {
-    sonido3.play();
-  });
